ca7f2406833c59aa91419e247c5bcb2bb0c44275
Ryan 'Thor' Kirkland <ryankirklandga@gmail.com>
2017-01-03 12:03:38 -0800

    Create, Read, Update, Destroy completed

--- app/controllers/wikis_controller.rb
+++ app/controllers/wikis_controller.rb
@@ -26,8 +26,32 @@ class WikisController < ApplicationController
   end
 
   def edit
+    @wiki = Wiki.find(params[:id])
+  end
+
+  def update
+    @wiki = Wiki.find(params[:id])
+    @wiki.title = params[:wiki][:title]
+    @wiki.body = params[:wiki][:body]
+
+    if @wiki.save
+      flash[:notice] = "Ya update was saved, bruh."
+      redirect_to @wiki
+    else
+      flash[:alert] = "Nope."
+      render :edit
+    end
   end
 
   def destroy
+    @wiki = Wiki.find(params[:id])
+
+    if @wiki.destroy
+      flash[:notice] = "IT'S GONE. IT'S ALL GONE."
+      redirect_to wikis_path
+    else
+      flash[:alert] = "WE CANNOT BE DESTROYED"
+      render :show
+    end
   end
 end
